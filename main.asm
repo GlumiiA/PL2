0b833f650a13d9442ad9701a475247e90fbfbf87
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2024-10-24 15:29:41 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,50 +1,46 @@
-%include "dict.inc"
-%include "words.inc"
-%include "lib.inc"
-
-%define BUF_SIZE 255
-%define SYS_READ 0
-%define STDIN 0
-%define STDERR 2
-%define SYS_WRITE 1
-
-global _start
-
-section .data
-
-err_msg: db "Key not found", 10, 0
+message_error_find: db "dictionary entry not found", 10, 0
+message_error_read: db "string is empty", 10, 0
 
 section .text
 
+global _start
+
 _start:
-	sub rsp, BUF_SIZE				; выделение на стеке места под строку с stdin
-	mov rax, SYS_READ
-	mov rdi, STDIN
-	mov rsi, rsp
-	mov rdx, BUF_SIZE
-	syscall
-
-	mov byte [rsp + rax - 1], 0 	; заменяем '\n' в конце прочитанной строки на 0-терм
-
-	mov rdi, rsp
-	mov rsi, DICT_BEGIN
-	call find_word					; поиск ключа в словаре
-
-	test rax, rax
-	jz .not_found
-
-	mov r12, rax					; в r12 сохраняем адрес начала вхождения
-	lea rdi, [rax + 8]
-	call string_length				; вычисление длины ключа
-	lea rdi, [r12 + 8 + rax + 1] 	; в rdi складываем значение по ключу
-	call print_string
-	xor rdi, rdi
-	jmp .exit
-
-.not_found:
-	call print_err
-
+;Читает строку размером не более 255 символов в буфер с stdin.
+; Пытается найти вхождение в словаре; если оно найдено, распечатывает в stdout значение по этому ключу.
+; Иначе выдает сообщение об ошибке.
+.read_string:
+    sub rsp, SIZE_BUFFER ; выделения места на стеке для строки
+    mov r12, rsp
+    sub rsp, 8 ; выравниваем стек
+    mov rax, SYS_READ
+    mov rdi, STDIN
+    mov rsi, rsp
+    mov rdx, SIZE_BUFFER
+    syscall
+    test rax, rax ; если кол-во прочитанных символов 0, то выход
+    jz .not_read
+    mov rdi, r12 ; Указатель на нуль-терминированную строку.
+    mov rsi, DICT_BEGIN ; Указатель на начало словаря.
+    call find_word
+    test rax, rax ; если не нашёл
+    jz .not_find
+.print_string:
+    lea rdi, [rdi + 8]
+    call string_length
+    lea rdi, [rdi + 8 + rax + 1]
+    ; значение по ключю в rdi
+    call print_string
+    jmp .exit
+.not_read:
+    mov rdi, message_error_read
+    call print_err
+    jmp .exit
+.not_find:
+    mov rdi, message_error_find
+    call print_err
 .exit:
-	add rsp, 255
-	call exit
-
+    add rsp, 8
+    add rsp, SIZE_BUFFER
+    xor rdi, rdi
+    call exit
