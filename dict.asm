d02aef13da3e617ebddfe649007339b4cdec7fe9
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2025-01-03 14:16:09 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,28 +1,39 @@
 %include "lib.inc"
-
 section .text
-
 global find_word
 
-; В rdi - адрес нуль-терминированной строки
-; В rsi - адрес начала словаря
+; Принимает два аргумента:
+; Указатель на нуль-терминированную строку.
+; Указатель на начало словаря.
+; Пройдёт по всему словарю в поисках подходящего ключа. Если подходящее вхождение найдено, вернёт
+; адрес начала вхождения в словарь (не значения), иначе вернёт 0.
 find_word:
-    push rdi
-    push rsi
-    lea rsi, [rsi + 8]
-    sub rsp, 8
-    call string_equals
-    add rsp, 8
-    pop rsi
-    pop rdi
-    cmp rax, 1
-    jne .skip
-    mov rax, rsi
-    ret
-    .skip:
-    mov rsi, [rsi]
-    test rsi, rsi
-    jnz find_word
-    xor rax, rax
+    sub rsp, 8 ; выравниваем стек
+.loop:
+    push rdi 
+    push rsi 
+
+    test rsi, rsi ; 
+    jz .out 
+
+    lea rsi, [rsi + 8] ; получаем адрес следующего элемента
+    call string_equals 
+
+    pop rsi 
+    pop rdi 
+
+    cmp rax, 1 ; проверяем результат
+    je .end ; если нашли ключ, переходим к .end
+
+    mov rsi, [rsi] ; переходим к следующему элементу
+    jmp .loop 
+
+.end:
+    add rsp, 8 
+    mov rax, rsi 
     ret
 
+.out:
+    add rsp, 8 ; восстанавливаем стек
+    xor rax, rax 
+    ret
