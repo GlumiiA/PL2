d7a29052d768e0becca309c411f52c30f37aec93
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2025-01-03 15:05:17 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -4,24 +4,41 @@ section .text
 
 global find_word
 
-; В rdi - адрес нуль-терминированной строки
-; В rsi - адрес начала словаря
+; Принимает два аргумента:
+; Указатель на нуль-терминированную строку.
+; Указатель на начало словаря.
+; Пройдёт по всему словарю в поисках подходящего ключа. Если подходящее вхождение найдено, вернёт
+; адрес начала вхождения в словарь (не значения), иначе вернёт 0.
 find_word:
     push rdi
     push rsi
+
+    test rsi, rsi
+    jz .out
+
     lea rsi, [rsi + 8]
+
     sub rsp, 8
     call string_equals
     add rsp, 8
+
+    cmp rax, 1  
+    je .found
+
     pop rsi
     pop rdi
-    cmp rax, 1
-    jne .skip
-    mov rax, rsi
-    ret
-    .skip:
     mov rsi, [rsi]
     test rsi, rsi
     jnz find_word
+    jmp .out
+
+.found:
+    mov rax, rsi
+    ret
+
+.out:
+    pop rsi
+    pop rdi    
     xor rax, rax
     ret
+
