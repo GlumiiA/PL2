3cf3693d29f487221817d214d1cd85999bf8f49b
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2025-01-03 14:47:08 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,33 +1,27 @@
 %include "lib.inc"
+
 section .text
+
 global find_word
 
+; В rdi - адрес нуль-терминированной строки
+; В rsi - адрес начала словаря
 find_word:
-
-.loop:
     push rdi
     push rsi
-    add rsi, 8  ; pointer to null-terminated string in rdi, beginning of dictionary in rsi
+    lea rsi, [rsi + 8]
     sub rsp, 8
-    call string_equals 
+    call string_equals
     add rsp, 8
     pop rsi
     pop rdi
     cmp rax, 1
-    je .found
-    mov rsi, [rsi] ; load the address of the next string in the dictionary -> rsi
-    cmp rsi, 0 ; 0 is the edn of dictionary
-    je .not_found
-    jmp .loop
-
-.found:
-    sub rsp, 8
-    call string_length 
-    add rsp, 8
-    add rax, rsi
-    add rax, 9 ; 8 for the next character and 1 for null terminator
+    jne .skip
+    mov rax, rsi
     ret
-
-.not_found:
-    mov rax, 0
+    .skip:
+    mov rsi, [rsi]
+    test rsi, rsi
+    jnz find_word
+    xor rax, rax
     ret
