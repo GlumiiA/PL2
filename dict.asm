ca0362a01343b4b7a1f1a38142d454199828bfd1
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2025-01-03 14:39:42 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,27 +1,43 @@
-%include "lib.inc"
-
 section .text
-
 global find_word
 
-; В rdi - адрес нуль-терминированной строки
-; В rsi - адрес начала словаря
 find_word:
-push rdi
-push rsi
-lea rsi, [rsi + 8]
-sub rsp, 8
-call string_equals
-add rsp, 8
-pop rsi
-pop rdi
-cmp rax, 1
-jne .skip
-mov rax, rsi
-ret
-.skip:
-mov rsi, [rsi]
-test rsi, rsi
-jnz find_word
-xor rax, rax
-ret
+    ; Сохраняем регистры
+    push rdi
+    push rsi
+    push rdx
+
+.loop:
+    ; Проверяем, не нулевой ли указатель на текущий элемент
+    test rsi, rsi
+    jz .not_found
+
+    ; Вызываем функцию сравнения строк
+    sub rsp, 8   ; Выравниваем стек перед вызовом
+    lea rdi, [rsi]  ; Указываем на текущий элемент
+    call string_equals
+    add rsp, 8   ; Восстанавливаем стек
+
+    ; Проверяем результат сравнения
+    cmp rax, 1   ; Проверяем, равно ли возвращаемое значение единице
+    je .found
+
+    ; Если не найдено, переходим к следующему элементу
+    mov rsi, [rsi] ; Получаем адрес следующего элемента
+    jmp .loop
+
+.found:
+    ; Если найдено, восстанавливаем регистры и возвращаем адрес
+    pop rdx
+    pop rsi
+    pop rdi
+    mov rax, rdi ; Возвращаем адрес найденного слова
+    ret
+
+.not_found:
+    ; Восстанавливаем регистры и возвращаем 0, если слово не найдено
+    pop rdx
+    pop rsi
+    pop rdi
+    xor rax, rax ; Устанавливаем rax в 0
+    ret
