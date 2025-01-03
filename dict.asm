3616944821fff1ee5792317a978376739a04cbd6
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2025-01-03 14:30:42 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,53 +1,27 @@
 %include "lib.inc"
+
 section .text
+
 global find_word
 
-; Принимает два аргумента:
-; Указатель на нуль-терминированную строку.
-; Указатель на начало словаря.
-; Пройдёт по всему словарю в поисках подходящего ключа. Если подходящее вхождение найдено, вернёт
-; адрес начала вхождения в словарь (не значения), иначе вернёт 0.
+; В rdi - адрес нуль-терминированной строки
+; В rsi - адрес начала словаря
 find_word:
-    ; Выравниваем стек
-    sub rsp, 8
-    
-    ; Сохраняем регистры
-    push rdi
-    push rsi
-    push rdx
-    
-.loop:
-    ; Проверяем, не нулевой ли указатель на текущий элемент
-    test rsi, rsi
-    jz .out
-
-    mov rdx, rsi
-
-    lea rsi, [rsi + 8]
-
-    call string_equals
-    ; Если строки равны, возвращаем адрес найденного слова
-    cmp rax, 1  
-    je .found
-
-    ; Если не найдено, продолжаем искать
-    mov rsi, [rdx] ; Переходим к следующему элементу в словаре
-    jmp .loop
-
-.found:
-    pop rdx
-    pop rsi
-    pop rdi
-    
-    mov rax, rdx ; Возвращаем адрес найденного слова
-    add rsp, 8   
-    ret
-
-.out:
-    pop rdx
-    pop rsi
-    pop rdi
-    
-    mov rax, 0  ; Возвращаем 0 при отсутствии слова
-    add rsp, 8 
-    ret
+push rdi
+push rsi
+lea rsi, [rsi + 8]
+sub rsp, 8
+call string_equals
+add rsp, 8
+pop rsi
+pop rdi
+cmp rax, 1
+jne .skip
+mov rax, rsi
+ret
+.skip:
+mov rsi, [rsi]
+test rsi, rsi
+jnz find_word
+xor rax, rax
+ret
