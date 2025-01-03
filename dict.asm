d9840bfe9dbe324c662b7bb9f596d3a174f4147e
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2025-01-03 15:08:15 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -10,35 +10,33 @@ global find_word
 ; Пройдёт по всему словарю в поисках подходящего ключа. Если подходящее вхождение найдено, вернёт
 ; адрес начала вхождения в словарь (не значения), иначе вернёт 0.
 find_word:
-    push rdi
     push rsi
+    push rdi
 
-    test rsi, rsi
-    jz .out
-
-    lea rsi, [rsi + 8]
+.loop:
+    mov rax, [rsi]
+    test rax, rax
+    jz .not_found
 
     sub rsp, 8
+    mov rdi, [rsi]
     call string_equals
     add rsp, 8
 
-    cmp rax, 1  
+    cmp rax, 1
     je .found
 
-    pop rsi
-    pop rdi
-    mov rsi, [rsi]
-    test rsi, rsi
-    jnz find_word
-    jmp .out
+    add rsi, 8
+    jmp .loop
 
 .found:
-    mov rax, rsi
-    ret
-
-.out:
+    mov rax, [rsi]
+    pop rdi
     pop rsi
-    pop rdi    
-    xor rax, rax
     ret
 
+.not_found:
+    xor rax, rax
+    pop rdi
+    pop rsi
+    ret
